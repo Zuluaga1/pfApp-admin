64d5baf982b126523d82152d998b8514d06ad34c
Esteban Zuluaga <estebanc.zuluaga@gmail.com>
2021-11-29 19:46:28 -0500

    entrega del primer apartado

--- app/src/main/java/com/example/admin/TCP_Sender.java
+++ app/src/main/java/com/example/admin/TCP_Sender.java
@@ -1,12 +1,16 @@
 package com.example.admin;
 
 import android.os.AsyncTask;
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
 
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
 
+@RequiresApi(api = Build.VERSION_CODES.CUPCAKE)
 public class TCP_Sender extends AsyncTask<String,Void,Void> {
     Socket server;
     DataOutputStream dos;
@@ -15,7 +19,7 @@ public class TCP_Sender extends AsyncTask<String,Void,Void> {
     protected Void doInBackground(String... voids) {
         String message = voids[0];
         try{
-            server = new Socket("192.168.1.85",10840);
+            server = new Socket("192.168.1.7",10840);
             pw = new PrintWriter(server.getOutputStream());
             pw.write(message);
             pw.flush();
